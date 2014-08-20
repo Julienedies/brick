da6f7a0d5f15727fa467dbc5c39badb46ed38cbd
Julienedies <julienedies@gmail.com>
2014-08-20 10:53:46 +0800

    refactor sub/pub part;

--- src/brick.js
+++ src/brick.js
@@ -1,5 +1,5 @@
 /**
- * Created by julien on 2014/6/30.
+ * Created by julien.zhang on 2014/6/30.
  *
  * 定义模型对象，UI组件，控制器对象的原型；
  *
@@ -60,14 +60,122 @@
 
     }
 
-    function eventManager(o){
-        o = o || {};
+    /*
+     * 事件管理器
+     */
+    var eventManager = (function(){
+
         var _events = {};
 
-        o.bind = function(){
+        return {
 
-        }
-    }
+            bind: function(e, f, context){
+
+                var handle = {f:f};
+
+                var event = this._getNamespace(e);
+
+                if(context){
+                    handle.context = context;
+                }
+
+                var callback = event._callback = event._callback || [];
+
+                callback.push(handle);
+            },
+
+            unbind: function(e, f){
+
+                var event = this._getNamespace(e);
+                var callback = event && event._callback;
+                var handle;
+
+                if(callback){
+
+                    if(!f){
+                        callback = [];
+                        return;
+                    }
+
+                    for(var i= 0, len = callback.length; i<len; i++){
+
+                        handle = callback[i];
+
+                        if(f.toString() === handle.f.toString()){
+                            event.splice(i,1);
+                            return;
+                        }
+
+                    }
+                }
+
+            },
+
+            fire: function(e, msg, flag){
+
+                var namespace = e.split('.');
+
+                var args;
+
+                var event = this._getNamespace(e);
+                var callback = event && event._callback;
+                var handle;
+                var context;
+                var f;
+
+                if(callback){
+
+                   // _cc('fire=> ', e, msg);
+
+                    for(var i= 0, len = callback.length; i<len; i++){
+
+                         handle = callback[i];
+                         context = handle.context || {};
+                         f = handle.f;
+
+                         args = Array.prototype.slice.call(arguments, 1);
+
+                         args.unshift({eventName:e});
+
+                         f.apply(context, args);
+
+                    }
+
+                }
+
+                if(flag || namespace.length<2) return;
+
+                namespace.pop();
+                e = namespace.join('.') + '.*';
+                this.fire(e, msg, 1);
+
+            },
+
+            _getNamespace: function(e){
+
+                return _events[e] = _events[e] || {};
+
+                var namespace = e.split('.');
+
+                return (function(k, _events){
+
+                    var i = k.shift();
+                    var o = _events[i] = _events[i] || {};
+
+                    if(k.length){
+                        return arguments.callee(k, o);
+                    }
+
+                    return o;
+
+                })(namespace, _events);
+
+            }
+
+
+        };
+
+    })();
 
 
     /*
@@ -125,14 +233,36 @@
             },
             _fire: function(e){
                 var selfName = this._name;
-                var arr = (_bind[selfName] || []).slice();
+                var arr = ( _bind[selfName] || [] ).slice();
+
                 for(var i; i = arr.shift(); ){
-                    _ctrls[i].scope._watch(extend({source:this}, e||{}));
+
+                    _ctrls[i].scope._watch( extend( {source:this}, e || {} ) );
+
                 }
             },
             _watch: function(e){
 
+            },
+
+            fire: function(e, msg){
+
+                eventManager.fire(e, msg);
+
+            },
+
+            watch: function(e, f){
+
+                var that = this;
+
+                eventManager.bind(e, f, that);
+
+            },
+
+            unbind: function(e, f){
+                eventManager.unbind(e, f);
             }
+
         });
 
 
@@ -144,7 +274,7 @@
         }
 
 
-        var _out = {
+        return {
             bind: _bind,
             /*
              * 获取一个控制器的对外接口对象
@@ -161,19 +291,19 @@
                 var depend = conf.depend || [];
                 var scope;
                 scope = parent ? f(name, _ctrls[parent]) : f(name);
-                _ctrls[name] = {fn:ctrl, scope:scope, depend:depend};
+                if(conf.global){
+                    window[name] = scope;
+                }
+                _ctrls[name] = {fn:ctrl, scope:scope, depend:depend, service:conf.service};
             },
             /*
              * 初始化控制器
              */
             init: function(name){
                 var ctrls = _ctrls;
-                if(name){
-                    ctrls = {};
-                    ctrls[name] = _ctrls[name];
-                }
                 var ctrl;
                 var depend;
+                var service;
                 for(var i in ctrls){
                     ctrl = ctrls[i];
                     depend = services.get(ctrl.depend) || [];
@@ -181,12 +311,16 @@
                         depend = [depend];
                     }
                     depend.unshift(ctrl.scope);
-                    ctrl.fn.apply(null, depend);
+                    service = ctrl.fn.apply(null, depend);
+
+                    if(ctrl.service){
+                        services.fill(i, service);
+                    }
+
                 }
             }
         };
 
-        return _out;
     })();
 
     /*
@@ -196,7 +330,7 @@
         var services = {};
         var registry = {};
 
-        var o = {
+        return {
             look: function(){
                 console.log(registry);
             },
@@ -212,11 +346,17 @@
             create: function(name){
                 var that = this;
                 var info = registry[name];
+
+                if(!info) return;
+
                 var depend = info.depend;
                 if(depend){
                     depend = that.get(depend);
                 }
                 return info.serve.apply(null, depend || []);
+            },
+            fill: function(name, service){
+                services[name] = service;
             },
             /*
              * 获取一个服务实例
@@ -225,11 +365,13 @@
                 var that = this;
                 if(!name) return;
 
+                //外部get
                 if(typeof name === 'string') {
                     return services[name] = services[name] || that.create(name);
                 }
 
-                if(name.slice && name.length){
+                //内部get
+                if(name.constructor === Array){
 
                     name = name.slice();
 
@@ -242,8 +384,6 @@
                 }
             }
         };
-
-        return o;
 
     })();
 
@@ -268,13 +408,46 @@
                     return this;
                 }
             };
+        },
+        /*
+         * 数字格式化
+         */
+        dFormat: function (d, opt) {
+            opt = opt || {};
+
+            var dg,
+                l,
+                r,
+                point,
+                reg,
+                ro;
+
+            dg = opt.dg * 1 || 0;
+            l = Math.pow(10, dg);
+            d = Math.round(d * l) / l;
+            r = (d + '').split('.');
+            point = r[1] || '';
+
+            reg = new RegExp('0{' + point.length + '}');
+
+            ro = _.range(dg);
+            _.each(ro, function (v, i, list) {
+                list[i] = 0;
+            });
+            ro = ro.join('');
+            point = ro.replace(reg, point);
+            d = r[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+            d = d + (point ? '.' + point : '');
+            return /^-/.test(d) ? d.replace(/\-(.+)$/, '($1)') : d;
         }
+
     };
 
     /*
      * 开发时的debug函数
      */
-    var isDevelop = 1;
+    var isDevelop = location.hostname == 'dev.com';
+
     var _cc = (isDevelop && window.console && (function(){
 
         return function () {
@@ -287,13 +460,15 @@
     })()) || function(){};
 
 
-//////////////////////////////////////
-// 对外接口
+    ////////////////////////////////////////////////
+    // 对外接口
+    ////////////////////////////////////////////////
 
     root.brick = {
         services: services,
         controllers: controllers,
-        utils: utils
+        utils: utils,
+        isDevelop: isDevelop
     };
 
     root._cc = _cc;
