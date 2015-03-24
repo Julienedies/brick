6a463542324f2de4cc95507f16916f168928d3b9
Julien.zhang <julien.zhang@chinascopefinancial>
2015-03-24 10:01:57 +0800

    add change event

--- static/js/brick/directives/event.js
+++ static/js/brick/directives/event.js
@@ -4,25 +4,31 @@
 
 directives.add('ic-event', function () {
 
+    var events = 'click,change';
 
-    var events = 'click';
-
-    var target = events.replace(/(?:^|,)(\w+?)(?=(?:,|$))/g, function (m, $1) {
+    var targets = events.replace(/(?:^|,)(\w+?)(?=(?:,|$))/g, function (m, $1) {
         var s = '[ic-?]'.replace('?', $1);
         return m.replace($1, s);
     });
 
+    var $doc = $('body');
 
-    $('body').on(events, target, function (e) {
+    events = events.split(',');
+    targets = targets.split(',');
 
+    _.forEach(events, function(event, i, list){
+        var target = targets[i];
+        $doc.on(event, target, _call);
+    });
+
+
+    function _call(e){
         var th = $(this);
         var type = e.type;
         var fn = th.attr('ic-' + type);
         fn = th.icParseProperty(fn);
 
         return fn.apply(this, [e]);
-
-    });
-
+    }
 
 });
