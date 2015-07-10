a9de0053ccbae34afdf06a907930b40d5b6071b7
Julien.zhang <julien@lulutrip.com>
2015-07-10 15:51:00 +0800

    add ic-checkbox

---
+++ src/widget/checkbox.js
@@ -0,0 +1,16 @@
+/**
+ * Created by rnd on 2015/7/10.
+ */
+
+
+directives.add('ic-widget-checkbox', function ($elm, attrs) {
+
+    var th = $elm;
+    var name = th.attr('ic-checkbox');
+
+
+
+
+
+});
+
