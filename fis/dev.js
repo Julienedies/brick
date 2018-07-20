1e5b0d168bb19b0f84b5f60e692169d41690a6b7
julienedies <julienedies@gamil.com>
2018-07-20 21:57:54 +0800

    fix fis

--- fis/dev.js
+++ fis/dev.js
@@ -86,7 +86,7 @@ fis.config.set('roadmap.path', [
 fis.config.merge({
     deploy: {
         ls: {
-            to: '../../jHandy/js/vendor/brick/' + version
+            to: '../../gushenwei.github.io/static/js/vendor/brick/' + version
         },
         local: {
             to: '../dist/' + version
