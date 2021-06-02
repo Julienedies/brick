547e1d570b169bc0c1b6f4bd3a393890f5f52813
julienedies <julienedies@gamil.com>
2021-06-02 09:07:27 +0800

    stage X

--- dist/brick.transition.js
+++ dist/brick.transition.js
@@ -2,8 +2,8 @@
  * https://github.com/julienedies/brick.git
  * https://github.com/Julienedies/brick/wiki
  * license:ISC
- * V0.9.32
- * 10/17/2020, 12:37:02 PM
+ * V0.9.33
+ * 10/17/2020, 1:10:22 PM
  * 
  */
 (function webpackUniversalModuleDefinition(root, factory) {
